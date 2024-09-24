3b633c961c9c35c81aec680d0d4e84466c31aafb
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-09-24 23:04:10 +0300

    lesson 35, fetch data and loading

--- kurslarim/kurslarim/src/App.js
+++ kurslarim/kurslarim/src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Courses from "./Courses";
+import Loading from "./Loading";
 
 function App() {
-  return <div className="App"></div>;
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchCourses = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get("http://localhost:3000/courses");
+      setCourses(response.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCourses();
+  }, []);
+  return (
+    <div className="App">
+      {loading ? <Loading /> : <Courses courses={courses} />}
+    </div>
+  );
 }
 
 export default App;
