9fec69c400df0013bbee4f5052d9fe05a79bef3a
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-09-26 22:56:54 +0300

    lesson 39, single card and icons

--- kurslarim/kurslarim/src/Courses.js
+++ kurslarim/kurslarim/src/Courses.js
@@ -1,6 +1,12 @@
 import Course from "./Course";
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 function Courses({ courses, removeCourse }) {
+  const [index, setIndex] = useState(1);
+
+  const { content, title, price } = courses[index];
+
   return (
     <div className="courseMainDiv">
       <div>
@@ -8,7 +14,22 @@ function Courses({ courses, removeCourse }) {
       </div>
 
       <div className="cardDiv">
-        {courses.map((course) => {
+        <button className="prevNextBtn">
+          <FaChevronLeft />
+        </button>
+        <div className="card">
+          <div className="cardTitlePrice">
+            <h2 className="cardTitle">{title}</h2>
+            <h4 className="cardPrice">{price} TL</h4>
+          </div>
+          <p>{content}</p>
+        </div>
+
+        <button className="prevNextBtn">
+          <FaChevronRight />
+        </button>
+
+        {/* {courses.map((course) => {
           return (
             <Course
               key={course.id}
@@ -16,7 +37,7 @@ function Courses({ courses, removeCourse }) {
               removeOneCourse={removeCourse}
             />
           );
-        })}
+        })} */}
       </div>
     </div>
   );
