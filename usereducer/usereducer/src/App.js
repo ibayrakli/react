8399600d784b51231b5e4b76a7fd2921bbe3cbb5
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-09-28 21:48:32 +0300

    lesson 42, use reduce implementation

--- usereducer/usereducer/src/App.js
+++ usereducer/usereducer/src/App.js
@@ -1,7 +1,50 @@
 import "./App.css";
+import { useReducer } from "react";
+
+const initialValue = 0;
+
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialValue;
+    default:
+      return state;
+  }
+};
 
 function App() {
-  return <div className="App"></div>;
+  const [count, dispatch] = useReducer(reducer, initialValue);
+
+  return (
+    <div className="App">
+      <div>Sayı = {count}</div>
+      <button
+        onClick={() => {
+          dispatch("increment");
+        }}
+      >
+        Arttır
+      </button>
+      <button
+        onClick={() => {
+          dispatch("decrement");
+        }}
+      >
+        Azalt
+      </button>
+      <button
+        onClick={() => {
+          dispatch("reset");
+        }}
+      >
+        Sıfırla
+      </button>
+    </div>
+  );
 }
 
 export default App;
