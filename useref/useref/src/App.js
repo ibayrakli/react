a69cca1020e9b60e91d2636356ad6843d63475e0
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-09-29 22:56:55 +0300

    lesson 46, useRef implementation

--- useref/useref/src/App.js
+++ useref/useref/src/App.js
@@ -1,23 +1,36 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
   const [name, setName] = useState("");
-  const [renderCount, setRenderCount] = useState(0);
+  // const [renderCount, setRenderCount] = useState(0);
+
+  const renderCount = useRef(0);
+
+  const inputRef = useRef();
 
   useEffect(() => {
-    setRenderCount((prev) => prev + 1);
+    // setRenderCount((prev) => prev + 1);
+    renderCount.current = renderCount.current + 1;
   });
+
+  const focusInput = () => {
+    console.log(inputRef.current);
+    inputRef.current.focus();
+    inputRef.current.value = "Test";
+  };
 
   return (
     <div className="App">
       <input
+        ref={inputRef}
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
       <div>Benim adım {name}</div>
-      <div>{renderCount} defa render oldu </div>
+      <div>{renderCount.current} defa render oldu </div>
+      <button onClick={focusInput}>Focus</button>
     </div>
   );
 }
