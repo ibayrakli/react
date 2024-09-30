bf04da53a99517e822cb9040b1627fdbf978d321
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-09-30 19:11:22 +0300

    lesson 48, useDeferredValue

--- usetransition/usetransition/src/App.js
+++ usetransition/usetransition/src/App.js
@@ -1,4 +1,6 @@
 import { useState, useTransition } from "react";
+import Characters from "./Characters";
+
 import "./App.css";
 
 function App() {
@@ -9,25 +11,32 @@ function App() {
 
   const handleChange = (e) => {
     setInput(e.target.value);
-
-    startTransition(() => {
-      const myArray = [];
-      for (let i = 0; i < 2000; i++) {
-        myArray.push(e.target.value);
-      }
-      setMyList(myArray);
-    });
   };
+
+  // const handleChange = (e) => {
+  //   setInput(e.target.value);
+
+  //   startTransition(() => {
+  //     const myArray = [];
+  //     for (let i = 0; i < 2000; i++) {
+  //       myArray.push(e.target.value);
+  //     }
+  //     setMyList(myArray);
+  //   });
+  // };
 
   return (
     <>
       <input type="text" value={input} onChange={handleChange} />
+      <Characters input={input} />
+
+      {/* <input type="text" value={input} onChange={handleChange} />
 
       {isPending
         ? "Loading...."
         : myList.map((item, index) => {
             return <div key={index}>{item}</div>;
-          })}
+          })} */}
     </>
   );
 }
