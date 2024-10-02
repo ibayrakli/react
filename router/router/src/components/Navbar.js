6df00ee5d52a59654b6a970633f22f9545dd0f4a
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-10-02 21:02:18 +0300

    lesson 54, navlink

--- router/router/src/components/Navbar.js
+++ router/router/src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <div>
       <nav>
-        <Link to="/"> Home</Link>
-        <Link to="/about"> About </Link>
+        <NavLink to="/"> Home</NavLink>
+        <NavLink to="/about"> About </NavLink>
       </nav>
     </div>
   );
