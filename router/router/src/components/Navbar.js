2cabb2e33ee49e312378b9b9b9b4bd64aea9cbb4
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-10-03 21:03:22 +0300

    lesson 56, nested route

--- router/router/src/components/Navbar.js
+++ router/router/src/components/Navbar.js
@@ -4,9 +4,10 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <div>
-      <nav>
+      <nav className="mainNav">
         <NavLink to="/"> Home</NavLink>
         <NavLink to="/about"> About </NavLink>
+        <NavLink to="/history"> History </NavLink>
       </nav>
     </div>
   );
