51ac57a66c4d086d7790ea3c860e7ba839c83dbb
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-10-07 18:39:12 +0300

    lesson 66, menu

---
+++ burger/burger/src/helpers/Data.js
@@ -0,0 +1,76 @@
+import BurgerImage from "../assets/burger.jpg";
+
+export const Data = [
+  {
+    name: "Efsane Burger",
+    image: BurgerImage,
+    content: "150 gr Kasap Burger, Patates, Turşu",
+    price: 250,
+  },
+  {
+    name: "Efsane Burger",
+    image: BurgerImage,
+    content: "150 gr Kasap Burger, Patates, Turşu",
+    price: 250,
+  },
+  {
+    name: "Efsane Burger",
+    image: BurgerImage,
+    content: "150 gr Kasap Burger, Patates, Turşu",
+    price: 250,
+  },
+  {
+    name: "Efsane Burger",
+    image: BurgerImage,
+    content: "150 gr Kasap Burger, Patates, Turşu",
+    price: 250,
+  },
+  {
+    name: "Efsane Burger",
+    image: BurgerImage,
+    content: "150 gr Kasap Burger, Patates, Turşu",
+    price: 250,
+  },
+  {
+    name: "Efsane Burger",
+    image: BurgerImage,
+    content: "150 gr Kasap Burger, Patates, Turşu",
+    price: 250,
+  },
+  {
+    name: "Efsane Burger",
+    image: BurgerImage,
+    content: "150 gr Kasap Burger, Patates, Turşu",
+    price: 250,
+  },
+  {
+    name: "Efsane Burger",
+    image: BurgerImage,
+    content: "150 gr Kasap Burger, Patates, Turşu",
+    price: 250,
+  },
+  {
+    name: "Efsane Burger",
+    image: BurgerImage,
+    content: "150 gr Kasap Burger, Patates, Turşu",
+    price: 250,
+  },
+  {
+    name: "Efsane Burger",
+    image: BurgerImage,
+    content: "150 gr Kasap Burger, Patates, Turşu",
+    price: 250,
+  },
+  {
+    name: "Efsane Burger",
+    image: BurgerImage,
+    content: "150 gr Kasap Burger, Patates, Turşu",
+    price: 250,
+  },
+  {
+    name: "Efsane Burger",
+    image: BurgerImage,
+    content: "150 gr Kasap Burger, Patates, Turşu",
+    price: 250,
+  },
+];
