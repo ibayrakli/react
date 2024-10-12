ace42513815e566d522556460378d1995fd1c14f
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-10-12 18:04:55 +0300

    lesson 72, yup validation

--- formik/formproject/src/components/GeneralForm.js
+++ formik/formproject/src/components/GeneralForm.js
@@ -1,18 +1,33 @@
 import React from "react";
 import { useFormik } from "formik";
+import { basicSchema } from "../schemas";
 
-function GeneralForm() {
-  const { values, errors, handleChange, handleSubmit } = useFormik({
-    initialValues: {
-      email: "",
-      age: "",
-      password: "",
-      confirmPassword: "",
-    },
+const onSubmit = async (values, actions) => {
+  console.log(values);
+  console.log(actions);
+
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1000);
   });
 
+  actions.resetForm();
+};
+
+function GeneralForm() {
+  const { values, errors, isSubmitting, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: {
+        email: "",
+        age: "",
+        password: "",
+        confirmPassword: "",
+      },
+      validationSchema: basicSchema,
+      onSubmit,
+    });
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="inputDiv">
         <label> Email</label>
         <input
@@ -21,7 +36,9 @@ function GeneralForm() {
           onChange={handleChange}
           id="email"
           placeholder="Mail adresinizi giriniz"
+          className={errors.email ? "input-error" : ""}
         />
+        {errors.email && <p className="error">{errors.email}</p>}
       </div>
       <div className="inputDiv">
         <label> Yaş</label>
@@ -31,7 +48,9 @@ function GeneralForm() {
           onChange={handleChange}
           id="age"
           placeholder="Yaşınızı giriniz"
+          className={errors.age ? "input-error" : ""}
         />
+        {errors.age && <p className="error">{errors.age}</p>}
       </div>
       <div className="inputDiv">
         <label>Şifre</label>
@@ -41,7 +60,9 @@ function GeneralForm() {
           onChange={handleChange}
           id="password"
           placeholder="Şifrenizi giriniz"
+          className={errors.password ? "input-error" : ""}
         />
+        {errors.password && <p className="error">{errors.password}</p>}
       </div>
       <div className="inputDiv">
         <label> Şifre Tekrar</label>
@@ -51,9 +72,15 @@ function GeneralForm() {
           onChange={handleChange}
           id="confirmPassword"
           placeholder="Şifrenizi tekrar giriniz"
+          className={errors.confirmPassword ? "input-error" : ""}
         />
+        {errors.confirmPassword && (
+          <p className="error">{errors.confirmPassword}</p>
+        )}
       </div>
-      <button type="submit">Kaydet</button>
+      <button disabled={isSubmitting} type="submit">
+        Kaydet
+      </button>
     </form>
   );
 }
