7614db46b3f5cf21cffadf116b2b45c67333170a
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-10-15 21:39:52 +0300

    lesson 79, datatype - number, string, boolean

--- typescript/1-Kurulum/app.js
+++ typescript/1-Kurulum/app.js
@@ -1,9 +1,30 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+// console.log("Typescript 1 2 3 5 6");
+// console.log("hello world!");
 var age = 2025;
 var firstName = "Name";
 var isUpdated = true;
 function display(id, name) {
     console.log("ID =" + id + " Name = " + name);
 }
-display(1, "Deneme");
+//display(1, "Deneme");
+// lesson 79, data-type-number-string-boolean
+// let first: number = 123;
+// let second: number = 0x37cf;
+// let third: number = 0o377;
+// let fourth: number = 0b111001;
+// console.log(first); //123
+// console.log(second); //14287
+// console.log(third); //255
+// console.log(fourth); //57
+// let employeefirstName: string = "Deneme";
+// let department: string = "Dept1";
+// console.log(employeefirstName);
+// //pre-es6
+// let desc1: string =
+//   employeefirstName + " works in the " + department + " department.";
+// //post-es6
+// let desc2: string = `${employeefirstName} works in the ${department} department.`;
+// console.log(desc1);
+// console.log(desc2);
+var isPresent = false;
+console.log(isPresent);
--- typescript/1-Kurulum/app.ts
+++ typescript/1-Kurulum/app.ts
@@ -1,6 +1,5 @@
 // console.log("Typescript 1 2 3 5 6");
 // console.log("hello world!");
-export {};
 
 let age: number = 2025;
 let firstName: string = "Name";
@@ -10,4 +9,32 @@ function display(id: number, name: string) {
   console.log("ID =" + id + " Name = " + name);
 }
 
-display(1, "Deneme");
+//display(1, "Deneme");
+
+// lesson 79, data-type-number-string-boolean
+// let first: number = 123;
+// let second: number = 0x37cf;
+// let third: number = 0o377;
+// let fourth: number = 0b111001;
+
+// console.log(first); //123
+// console.log(second); //14287
+// console.log(third); //255
+// console.log(fourth); //57
+
+// let employeefirstName: string = "Deneme";
+// let department: string = "Dept1";
+// console.log(employeefirstName);
+
+// //pre-es6
+// let desc1: string =
+//   employeefirstName + " works in the " + department + " department.";
+
+// //post-es6
+// let desc2: string = `${employeefirstName} works in the ${department} department.`;
+
+// console.log(desc1);
+// console.log(desc2);
+
+let isPresent: boolean = false;
+console.log(isPresent);
