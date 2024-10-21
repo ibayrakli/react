03138990db6e53dddda1e8f45bd612986c68017e
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-10-21 20:04:28 +0300

    lesson 97, rest parameters

--- typescript/1-Kurulum/app.ts
+++ typescript/1-Kurulum/app.ts
@@ -328,12 +328,27 @@ function display(id: number, name: string) {
 
 // printToConsole();
 
-function add(a: string, b: string): string;
-function add(a: number, b: number): number;
+// function add(a: string, b: string): string;
+// function add(a: number, b: number): number;
 
-function add(a: any, b: any): any {
-  return a + b;
+// function add(a: any, b: any): any {
+//   return a + b;
+// }
+
+// let result = add(3, 5);
+// console.log(result);
+
+// function toplam(a: string, ...numbers: number[]): number {
+//   console.log(a);
+//   let total = 0;
+//   numbers.forEach((num) => (total += num));
+//   return total;
+// }
+
+// console.log(toplam("Sum", 5, 10, 15));
+
+function merge(message: string, ...names: string[]) {
+  console.log(message + " " + names.join(", "));
 }
 
-let result = add(3, 5);
-console.log(result);
+merge("Hi", "Firstname", "Middlename", "Surname");
