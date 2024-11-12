18a1b072d555770fb0edf5c6bf13e9c847d170d1
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-11-12 19:35:25 +0300

    lesson 135, multiselect

--- materialui/materialui/src/components/LessonSelect.tsx
+++ materialui/materialui/src/components/LessonSelect.tsx
@@ -2,17 +2,43 @@ import { Box, TextField, MenuItem } from "@mui/material";
 import { useState } from "react";
 
 export const LessonSelect = () => {
-  const [country, setCountry] = useState("");
+  // const [country, setCountry] = useState("");
+  // console.log(country);
+
+  const [country, setCountry] = useState<string[]>([]);
   console.log(country);
 
   return (
     <Box width="250px">
-      <TextField
+      {/* <TextField
         label="Select Country"
         select
         fullWidth
         value={country}
         onChange={(e) => setCountry(e.target.value)}
+      >
+        <MenuItem value="TR">Türkiye</MenuItem>
+        <MenuItem value="USA">USA</MenuItem>
+        <MenuItem value="FR">France</MenuItem>
+      </TextField> */}
+
+      <TextField
+        slotProps={{
+          select: {
+            multiple: true,
+          },
+        }}
+        label="Select Country"
+        select
+        fullWidth
+        value={country}
+        onChange={(e) =>
+          setCountry(
+            typeof e.target.value == "string"
+              ? e.target.value.split(",")
+              : e.target.value
+          )
+        }
       >
         <MenuItem value="TR">Türkiye</MenuItem>
         <MenuItem value="USA">USA</MenuItem>
