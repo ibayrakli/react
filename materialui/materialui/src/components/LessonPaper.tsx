56fc7c26b8121b6e7accf4e225d83dd1a0d16b8d
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-11-19 23:47:40 +0300

    lesson 146, paper

--- materialui/materialui/src/components/LessonPaper.tsx
+++ materialui/materialui/src/components/LessonPaper.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+import { Paper, Box } from "@mui/material";
 
 export const LessonPaper = () => {
-  return <div>LessonPaper</div>;
+  return (
+    <Box sx={{ display: "flex", gap: "20px" }}>
+      <Paper sx={{ width: 100, height: 100 }} />
+      <Paper sx={{ width: 100, height: 100 }} elevation={0} />
+      <Paper sx={{ width: 100, height: 100 }} elevation={1} />
+      <Paper sx={{ width: 100, height: 100 }} elevation={2} />
+      <Paper sx={{ width: 100, height: 100 }} elevation={3} />
+      <Paper sx={{ width: 100, height: 100 }} variant="outlined" />
+      <Paper sx={{ width: 100, height: 100 }} variant="outlined" square />
+    </Box>
+  );
 };
