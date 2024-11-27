9df1084ab898a51ab447721ca8b13bb3f10d758e
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-11-27 19:26:49 +0300

    lesson 153, avatar

--- materialui/materialui/src/components/LessonAvatar.tsx
+++ materialui/materialui/src/components/LessonAvatar.tsx
@@ -1,5 +1,127 @@
-import React from "react";
+import { Stack, AvatarGroup, Avatar } from "@mui/material";
 
 export const LessonAvatar = () => {
-  return <div>LessonAvatar</div>;
+  return (
+    <Stack direction="row" spacing={4}>
+      <AvatarGroup>
+        <Avatar
+          sx={{ marginRight: 10 }}
+          src="https://randomuser.me/api/portraits/women/56.jpg"
+        />
+        <Avatar src="https://randomuser.me/api/portraits/men/56.jpg" />
+      </AvatarGroup>
+      <AvatarGroup max={4}>
+        <Avatar
+          variant="square"
+          sx={{
+            marginRight: 10,
+            bgcolor: "primary.light",
+            width: 50,
+            height: 50,
+          }}
+        >
+          Jane
+        </Avatar>
+        <Avatar
+          variant="rounded"
+          sx={{
+            marginRight: 10,
+            bgcolor: "success.light",
+            width: 50,
+            height: 50,
+          }}
+        >
+          John
+        </Avatar>
+        <Avatar
+          variant="square"
+          sx={{
+            marginRight: 10,
+            bgcolor: "primary.light",
+            width: 50,
+            height: 50,
+          }}
+        >
+          Jane
+        </Avatar>
+        <Avatar
+          variant="rounded"
+          sx={{
+            marginRight: 10,
+            bgcolor: "success.light",
+            width: 50,
+            height: 50,
+          }}
+        >
+          John
+        </Avatar>
+        <Avatar
+          variant="square"
+          sx={{
+            marginRight: 10,
+            bgcolor: "primary.light",
+            width: 50,
+            height: 50,
+          }}
+        >
+          Jane
+        </Avatar>
+        <Avatar
+          variant="rounded"
+          sx={{
+            marginRight: 10,
+            bgcolor: "success.light",
+            width: 50,
+            height: 50,
+          }}
+        >
+          John
+        </Avatar>
+        <Avatar
+          variant="square"
+          sx={{
+            marginRight: 10,
+            bgcolor: "primary.light",
+            width: 50,
+            height: 50,
+          }}
+        >
+          Jane
+        </Avatar>
+        <Avatar
+          variant="rounded"
+          sx={{
+            marginRight: 10,
+            bgcolor: "success.light",
+            width: 50,
+            height: 50,
+          }}
+        >
+          John
+        </Avatar>
+        <Avatar
+          variant="square"
+          sx={{
+            marginRight: 10,
+            bgcolor: "primary.light",
+            width: 50,
+            height: 50,
+          }}
+        >
+          Jane
+        </Avatar>
+        <Avatar
+          variant="rounded"
+          sx={{
+            marginRight: 10,
+            bgcolor: "success.light",
+            width: 50,
+            height: 50,
+          }}
+        >
+          John
+        </Avatar>
+      </AvatarGroup>
+    </Stack>
+  );
 };
