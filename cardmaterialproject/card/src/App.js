1d51f18ad6ddd8da71600049aef3c86dde18a273
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-12-07 21:57:05 +0300

    lesson 170, appbar

--- cardmaterialproject/card/src/App.js
+++ cardmaterialproject/card/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <AppBar>
+        <Toolbar sx={{ fontSize: 30 }}>Card Project</Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default App;
