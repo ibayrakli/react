ecce61ca97f11c99e317d7847dd6b57117386330
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-12-09 18:50:55 +0300

    lesson 172, card

--- cardmaterialproject/card/src/Course.js
+++ cardmaterialproject/card/src/Course.js
@@ -1,10 +1,21 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
 function Course({ image, title, description }) {
   return (
-    <div>
-      <img src={image} alt="" />
-      <p>{title}</p>
-      <p>{description}</p>
-    </div>
+    <Card>
+      <CardMedia sx={{ height: 140 }} image={image} alt="course" />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {description}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
 
