3127eb7e4601860b8df081433a19e5cc31b97bac
ibayrakli <11743129+ibayrakli@users.noreply.github.com>
2024-12-16 21:18:27 +0300

    lesson 183, createSlice

--- redux/mycourses/src/store.js
+++ redux/mycourses/src/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cardReducers from "./control/cardSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    card: cardReducers,
+  },
 });
